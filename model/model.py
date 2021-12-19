7f14a4f797fd9652e75fffa063b96d4dfe7af773
amelie-ort <ortunoamelie@gmail.com>
2021-12-19 19:54:36 +0100

    fix

--- model/model.py
+++ model/model.py
@@ -9,11 +9,7 @@ class Model():
         
         self.data_formatting(data)
 
-        x = self.train_data.drop('HasWin', 1)
-        y = self.train_data['HasWin'].astype('category').cat.codes
-
-        self.modele_logit = LogisticRegression(solver="newton-cg")
-        self.modele_logit.fit(x,y)
+        self.create_model()
 
     def data_formatting(self, data):
         formatted_data = data[["N1", "N2", "N3", "N4", "N5", "E1", "E2"]].assign(HasWin = np.ones(data.shape[0], dtype=int))
@@ -26,6 +22,13 @@ class Model():
         total_data = pd.concat([formatted_data, false_data])
 
         self.train_data, self.test_data = train_test_split(total_data, test_size=0.2)
+
+    def create_model(self): 
+        x = self.train_data.drop('HasWin', 1)
+        y = self.train_data['HasWin'].astype('category').cat.codes
+
+        self.modele_logit = LogisticRegression(solver="newton-cg")
+        self.modele_logit.fit(x,y)
 
     def test(self):
         error = np.mean(self.modele_logit.predict(self.test_data.drop('HasWin', 1)) != self.test_data['HasWin'].to_numpy())
